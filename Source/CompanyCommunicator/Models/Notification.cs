9270666735ed9301cebc21ba6056e0b71bca2d4c
Hong Xu <v-hongxu@microsoft.com>
2019-07-01 16:44:29 -0700

    Add initial services

---
+++ Source/CompanyCommunicator/Models/Notification.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="Notification.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Models
+{
+    using System;
+
+    /// <summary>
+    /// Message model class.
+    /// </summary>
+    public class Notification
+    {
+        /// <summary>
+        /// Gets or sets Title value.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets Date value.
+        /// </summary>
+        public string Date { get; set; }
+
+        /// <summary>
+        /// Gets or sets Recipients value.
+        /// </summary>
+        public string Recipients { get; set; }
+
+        /// <summary>
+        /// Gets or sets Acknowledgements value.
+        /// </summary>
+        public string Acknowledgements { get; set; }
+
+        /// <summary>
+        /// Gets or sets Reactions value.
+        /// </summary>
+        public string Reactions { get; set; }
+
+        /// <summary>
+        /// Gets or sets Responses value.
+        /// </summary>
+        public string Responses { get; set; }
+    }
+}
