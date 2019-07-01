5dff8490ca9ef0beae5ca835576dd10612b4c519
Hong Xu <v-hongxu@microsoft.com>
2019-07-01 10:30:06 -0700

    Fix a typo

--- Source/CompanyCommunicator/Startup.cs
+++ Source/CompanyCommunicator/Startup.cs
@@ -2,7 +2,7 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-namespace CompanyCommunicator.Bot
+namespace CompanyCommunicator
 {
     using CompanyCommunicator.Bot;
     using Microsoft.AspNetCore.Builder;
