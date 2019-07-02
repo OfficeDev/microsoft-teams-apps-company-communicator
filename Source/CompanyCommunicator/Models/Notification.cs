948a5b6fa5cd2024ca84cc8b3773d7cb92699597
Hong Xu <v-hongxu@microsoft.com>
2019-07-02 12:18:58 -0700

    Fix namespace errors

--- Source/CompanyCommunicator/Models/Notification.cs
+++ Source/CompanyCommunicator/Models/Notification.cs
@@ -2,7 +2,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // </copyright>
 
-namespace Microsoft.Teams.Apps.CompanyCommunicator.Models
+namespace CompanyCommunicator.Models
 {
     using System;
 
