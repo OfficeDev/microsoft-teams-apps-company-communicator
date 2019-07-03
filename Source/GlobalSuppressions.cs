928873ff67b7c8bd5d9abaecfce28526642eb1ae
Hong Xu <v-hongxu@microsoft.com>
2019-07-03 16:32:31 -0700

    Remove commented out rule.

--- Source/GlobalSuppressions.cs
+++ Source/GlobalSuppressions.cs
@@ -6,5 +6,3 @@
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
-
-// [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1629:Documentation text should end with a period", Justification = "Convention")]
