5bde89ad7705cacccf3661a663e9601025d58c39
Hong Xu <v-hongxu@microsoft.com>
2019-07-05 10:09:40 -0700

    1). Added server side auth. 2). Set up MustHaveUpnPolicy in the MVC middleware.

---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator/Auth/MustContainUpnClaimRequirement.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="MustContainUpnClaimRequirement.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Auth
+{
+    using Microsoft.AspNetCore.Authorization;
+
+    /// <summary>
+    /// This class is an authorization policy requirement.
+    /// It specifies that an id token must contain Upn claim.
+    /// </summary>
+    public class MustContainUpnClaimRequirement : IAuthorizationRequirement
+    {
+    }
+}
---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator/Auth/MustContainUpnHandler.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="MustContainUpnHandler.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Auth
+{
+    using System.Linq;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+
+    /// <summary>
+    /// This class is an authorization handler.
+    /// It handles the authorization requirement, MustContainUpnClaimRequirement.
+    /// </summary>
+    public class MustContainUpnHandler : AuthorizationHandler<MustContainUpnClaimRequirement>
+    {
+        /// <summary>
+        /// This method handles the authorization requirement, MustContainUpnClaimRequirement.
+        /// </summary>
+        /// <param name="context">AuthorizationHandlerContext instance.</param>
+        /// <param name="requirement">MustContainUpnClaimRequirement instance.</param>
+        /// <returns>A task that represents the work queued to execute.</returns>
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            MustContainUpnClaimRequirement requirement)
+        {
+            var claim = context.User.Claims.FirstOrDefault(p => p.Type == ClaimTypes.Upn);
+            if (claim != null && this.Validate(claim.Value))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Validate a upn value.
+        /// </summary>
+        /// <param name="upn">Upn value.</param>
+        /// <returns>Indicate if a upn is valid or not.</returns>
+        private bool Validate(string upn)
+        {
+            return string.IsNullOrWhiteSpace(upn);
+        }
+    }
+}
