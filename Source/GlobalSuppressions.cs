3593891335c2b51c84eb320a86c2d8f42d7414b1
Hong Xu <v-hongxu@microsoft.com>
2019-07-08 11:18:35 -0700

    Use the same suppressio rules (and format) as the project microsoft-teams-list-search-app.

--- Source/GlobalSuppressions.cs
+++ Source/GlobalSuppressions.cs
@@ -2,7 +2,4 @@
 // Copyright (c) Microsoft. All rights reserved.
 // </copyright>
 
-// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1629:Documentation text should end with a period", Justification = "Convention")]
