26c8647ee70dbdd2530b9729ebb28b6963c23b9d
amyzhao <v-yuzha4@microsoft.com>
2019-07-15 11:52:08 -0700

    Create new message task Module

--- Source/Microsoft.Teams.Apps.CompanyCommunicator/ClientApp/src/components/NewMessage/newMessage.tsx
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator/ClientApp/src/components/NewMessage/newMessage.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
 import './newMessage.scss';
+import './teamTheme.scss';
 import { Input, TextArea } from 'msteams-ui-components-react';
 import * as AdaptiveCards from "adaptivecards";
+import { Dropdown, Checkbox } from 'msteams-ui-components-react';
+import { Button } from '@stardust-ui/react';
+import * as microsoftTeams from "@microsoft/teams-js";
 
 export interface formState {
     title: string,
-    summary: string,
-    articleLink?: string,
+    summary?: string,
+    btnLink?: string,
     imageLink?: string,
-    attachmentLink?: string,
-    card?: any
+    btnTitle?: string,
+    author: string,
+    card?: any,
+    onNext: boolean,
+    channel?: string,
+    team?: string,
+    channelBtn: boolean,
+    teamBtn: boolean,
+    allusersBtn: boolean
 }
 
 export default class NewMessage extends React.Component<{}, formState> {
@@ -23,156 +34,249 @@ export default class NewMessage extends React.Component<{}, formState> {
             "body": [
                 {
                     "type": "TextBlock",
-                    "size": "Large",
                     "weight": "Bolder",
-                    "color": "Dark",
-                    "text": "Title",
+                    "text": "",
+                    "size": "ExtraLarge",
+                    "wrap": true
                 },
                 {
                     "type": "Image",
                     "spacing": "Default",
-                    "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Seattle_monorail01_2008-02-25.jpg/1024px-Seattle_monorail01_2008-02-25.jpg",
+                    "url": "",
                     "size": "Stretch",
                     "width": "400px",
                     "altText": ""
                 },
                 {
                     "type": "TextBlock",
-                    "spacing": "Large",
-                    "separator": true,
-                    "height": "stretch",
-                    "weight": "Bolder",
-                    "text": "Summary",
-                    "maxLines": 30,
+                    "text": "",
+                    "wrap": true
+                },
+                {
+                    "type": "TextBlock",
                     "wrap": true,
-                    "width": "400px",
+                    "size": "Small",
+                    "weight": "Lighter",
+                    "text": "Sent by:"
                 }
             ],
             "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
             "version": "1.0"
-        };
+        }
 
         this.state = {
             title: "",
             summary: "",
-            card: {
-                "type": "AdaptiveCard",
-                "body": [
-                    {
-                        "type": "TextBlock",
-                        "size": "Large",
-                        "weight": "Bolder",
-                        "color": "Dark",
-                        "text": "Title",
-                    },
-                    {
-                        "type": "Image",
-                        "spacing": "Default",
-                        "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Seattle_monorail01_2008-02-25.jpg/1024px-Seattle_monorail01_2008-02-25.jpg",
-                        "size": "Stretch",
-                        "width": "400px",
-                        "altText": ""
-                    },
-                    {
-                        "type": "TextBlock",
-                        "spacing": "Large",
-                        "separator": true,
-                        "height": "stretch",
-                        "weight": "Bolder",
-                        "text": "Summary",
-                        "maxLines": 30,
-                        "wrap": true,
-                        "width": "400px",
-                    }
-                ],
-                "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-                "version": "1.0"
-            }
+            author: "",
+            btnLink: "",
+            imageLink: "",
+            btnTitle: "",
+            card: this.card,
+            onNext: false,
+            channel: "Team",
+            team: "Team",
+            channelBtn: false,
+            teamBtn: false,
+            allusersBtn: false
         }
     }
 
     public componentDidMount() {
+        microsoftTeams.initialize();
+        //- Handle the Esc key
+        document.addEventListener("keydown", this.escFunction, false);
         let adaptiveCard = new AdaptiveCards.AdaptiveCard();
         adaptiveCard.parse(this.state.card);
         let renderedCard = adaptiveCard.render();
         document.getElementsByClassName('adaptiveCardContainer')[0].appendChild(renderedCard);
+        let link = this.state.btnLink;
+        adaptiveCard.onExecuteAction = function (action) { window.open(link, '_blank'); }
+    }
+
+    public componentWillUnmount() {
+        document.removeEventListener("keydown", this.escFunction, false);
     }
 
     public render(): JSX.Element {
-        return (
-            <div className="taskModule">
-                <div className="formContainer">
-                    <div className="formContentContainer" >
-                        <Input
-                            className="inputField"
-                            value={this.state.title}
-                            label="Title"
-                            placeholder="Title"
-                            errorLabel={!this.state.title ? "This value is required" : undefined}
-                            onChange={this.onValueChanged}
-                            status={this.state.title ? "updated" : undefined}
-                            autoComplete="off"
-                            required
-                        />
+        if (this.state.onNext) {
+            return (
+                <div className="taskModule">
+                    <div className="formContainer">
 
-                        <TextArea
-                            className="inputField textArea"
-                            autoFocus
-                            placeholder="Summary"
-                            label="Summary"
-                            errorLabel={!this.state.summary ? "This value is required" : undefined}
-                            value={this.state.summary}
-                            onChange={this.onSummaryChanged}
-                            required />
+                        <h3>Recipient Selection</h3>
+                        <h4>Please choose the groups you would like to send your message to.</h4>
 
-                        <Input
-                            className="inputField"
-                            value={this.state.articleLink}
-                            label="Article Link"
-                            placeholder="Article link (optional)"
-                            onChange={this.onArticleLinkChanged}
-                            status={this.state.articleLink ? "updated" : undefined}
-                            autoComplete="off"
-                        />
+                        <div className="checkboxBtns">
+                            <p className="checkboxBtn">
+                                <Checkbox checked={this.state.channelBtn} label="Send to a Team(s)" value="teamtest" onChecked={this.onChannel} />
+                            </p>
 
-                        <Input
-                            className="inputField"
-                            value={this.state.imageLink}
-                            label="Image Link"
-                            placeholder="Image link (optional)"
-                            onChange={this.onImageLinkChanged}
-                            status={this.state.imageLink ? "updated" : undefined}
-                            autoComplete="off"
-                        />
+                            <p className="checkboxBtn">
+                                <Checkbox checked={this.state.teamBtn} label="Send to the team members of a Team(s)" value="teams" onChecked={this.onTeam} />
+                            </p>
 
-                        <Input
-                            className="inputField"
-                            value={this.state.attachmentLink}
-                            label="Attachment Link"
-                            placeholder="Attachment link (optional)"
-                            onChange={this.onAttachmentLinkChanged}
-                            status={this.state.attachmentLink ? "updated" : undefined}
-                            autoComplete="off"
-                        />
+                            <p className="checkboxBtn">
+                                <Checkbox checked={this.state.allusersBtn} label="Send to all users" value="users" onChecked={this.onAlluser} />
+                            </p>
+                        </div>
+
+                        <div className="boardSelection">
+                            <Dropdown
+                                className="dropDown"
+                                autoFocus
+                                mainButtonText={this.state.channel}
+                                style={{ width: '50%' }}
+                                items={[
+                                    { text: 'Team 1', onClick: () => { this.setState({ channel: "Team 1" }) } },
+                                    { text: 'Team 2', onClick: () => { this.setState({ channel: "Team 2" }) } }
+                                ]}
+                            />
+
+                            <Dropdown
+                                className="dropDown"
+                                autoFocus
+                                mainButtonText={this.state.team}
+                                style={{ width: '50%' }}
+                                items={[
+                                    { text: 'Team 1', onClick: () => { this.setState({ team: "Team 1" }) } },
+                                    { text: 'Team 2', onClick: () => { this.setState({ team: "Team 2" }) } }
+                                ]}
+                            />
+                        </div>
                     </div>
-                    <div className="adaptiveCardContainer">
+
+                    <div className="footerContainer">
+                        <div className="buttonContainer">
+                            <Button content="Back" onClick={this.onBack} secondary />
+                            <Button content="Save" disabled={!(this.state.channelBtn || this.state.teamBtn || this.state.allusersBtn)} id="saveBtn" onClick={this.onSave} primary />
+                        </div>
                     </div>
                 </div>
+            );
+        } else {
+            return (
+                <div className="taskModule">
+                    <div className="formContainer">
+                        <div className="formContentContainer" >
+                            <Input
+                                className="inputField"
+                                value={this.state.title}
+                                label="Title"
+                                placeholder="Title"
+                                errorLabel={!this.state.title ? "This value is required" : undefined}
+                                onChange={this.onValueChanged}
+                                status={this.state.title ? "updated" : undefined}
+                                autoComplete="off"
+                                required
+                            />
 
-                <div className="footerContainer">
-                    <div className="buttonContainer">
-                        <button className="secondaryBtn">Back</button>
-                        <button className="secondaryBtn">Save as draft</button>
-                        <button className="primaryBtn" disabled={this.state.title === "" || this.state.summary === ""}>Schedule</button>
+                            <Input
+                                className="inputField"
+                                value={this.state.imageLink}
+                                label="Image Link"
+                                placeholder="Image link (optional)"
+                                onChange={this.onImageLinkChanged}
+                                status={this.state.imageLink ? "updated" : undefined}
+                                autoComplete="off"
+                            />
+
+                            <TextArea
+                                className="inputField textArea"
+                                autoFocus
+                                placeholder="Summary (optional)"
+                                label="Summary"
+                                value={this.state.summary}
+                                onChange={this.onSummaryChanged}
+                            />
+
+                            <Input
+                                className="inputField"
+                                value={this.state.author}
+                                label="Author"
+                                placeholder="Author"
+                                errorLabel={!this.state.author ? "This value is required" : undefined}
+                                onChange={this.onAuthorChanged}
+                                status={this.state.author ? "updated" : undefined}
+                                autoComplete="off"
+                                required
+                            />
+
+                            <Input
+                                className="inputField"
+                                value={this.state.btnTitle}
+                                label="Button Title"
+                                placeholder="Button title"
+                                onChange={this.onbtnTitleChanged}
+                                status={this.state.btnTitle ? "updated" : undefined}
+                                autoComplete="off"
+                            />
+
+                            <Input
+                                className="inputField"
+                                value={this.state.btnLink}
+                                label="Button Url"
+                                placeholder="Button url"
+                                onChange={this.onbtnLinkChanged}
+                                status={this.state.btnLink ? "updated" : undefined}
+                                autoComplete="off"
+                            />
+                        </div>
+                        <div className="adaptiveCardContainer">
+                        </div>
+                    </div>
+
+                    <div className="footerContainer">
+                        <div className="buttonContainer">
+                            <Button content="Next" disabled={this.state.title === "" || this.state.author === ""} id="saveBtn" onClick={this.onNext} primary />
+                        </div>
                     </div>
                 </div>
-            </div>
+            );
+        }
+    }
 
-        );
+    private onSave = () => {
+        microsoftTeams.tasks.submitTask();
+    }
+
+    public escFunction(event: any) {
+        if (event.keyCode === 27 || (event.key === "Escape")) {
+            microsoftTeams.tasks.submitTask();
+        }
+    }
+
+    private onAlluser = () => {
+        this.setState({
+            allusersBtn: !this.state.allusersBtn
+        })
+    }
+
+    private onTeam = () => {
+        this.setState({
+            teamBtn: !this.state.teamBtn
+        })
+    }
+
+    private onChannel = (checked: boolean, value?: any) => {
+        this.setState({
+            channelBtn: !this.state.channelBtn
+        })
+    }
+
+    private onNext = (event: any) => {
+        this.setState({ onNext: !this.state.onNext });
+    }
+
+    private onBack = (event: any) => {
+        this.setState({
+            onNext: !this.state.onNext
+        }, () => {
+            this.updateCard();
+        });
     }
 
     private onValueChanged = (event: any) => {
-        this.card.body[0].text = event.target.value;
+        this.card.body[0].text = "" + event.target.value;
         this.setState({
             title: event.target.value,
             card: this.card
@@ -182,7 +286,7 @@ export default class NewMessage extends React.Component<{}, formState> {
     }
 
     private onSummaryChanged = (event: any) => {
-        this.card.body[2].text = event.target.value;
+        this.card.body[2].text = "" + event.target.value;
         this.setState({
             summary: event.target.value,
             card: this.card
@@ -191,16 +295,79 @@ export default class NewMessage extends React.Component<{}, formState> {
         });
     }
 
-    private onArticleLinkChanged = (event: any) => {
-        this.setState({ articleLink: event.target.value });
+    private onAuthorChanged = (event: any) => {
+        this.card.body[3].text = "Sent by : " + event.target.value;
+        this.setState({
+            author: event.target.value,
+            card: this.card
+        }, () => {
+            this.updateCard();
+        });
+    }
+
+
+    private onbtnLinkChanged = (event: any) => {
+
+        if (event.target.value !== "" && this.state.btnTitle !== "") {
+            this.card.actions = [
+                {
+                    "type": "Action.OpenUrl",
+                    "title": this.state.btnTitle,
+                    "url": event.target.value
+                }
+            ];
+
+            this.setState({
+                btnLink: event.target.value,
+                card: this.card
+            }, () => {
+                this.updateCard();
+            });
+        } else {
+            delete this.card.actions;
+            this.setState({
+                btnLink: event.target.value
+            }, () => {
+                this.updateCard();
+            });
+        }
+
     }
 
     private onImageLinkChanged = (event: any) => {
-        this.setState({ imageLink: event.target.value });
+        this.card.body[1].url = event.target.value;
+        this.setState({
+            imageLink: event.target.value,
+            card: this.card
+        }, () => {
+            this.updateCard();
+        });
     }
 
-    private onAttachmentLinkChanged = (event: any) => {
-        this.setState({ attachmentLink: event.target.value });
+    private onbtnTitleChanged = (event: any) => {
+        if (this.state.btnLink !== "" && event.target.value !== "") {
+            this.card.actions = [
+                {
+                    "type": "Action.OpenUrl",
+                    "title": event.target.value,
+                    "url": this.state.btnLink
+                }
+            ];
+
+            this.setState({
+                btnTitle: event.target.value,
+                card: this.card
+            }, () => {
+                this.updateCard();
+            });
+        } else {
+            delete this.card.actions;
+            this.setState({
+                btnTitle: event.target.value,
+            }, () => {
+                this.updateCard();
+            });
+        }
     }
 
     private updateCard = () => {
@@ -213,5 +380,7 @@ export default class NewMessage extends React.Component<{}, formState> {
         } else {
             document.getElementsByClassName('adaptiveCardContainer')[0].appendChild(renderedCard);
         }
+        let link = this.state.btnLink;
+        adaptiveCard.onExecuteAction = function (action) { window.open(link, '_blank'); }
     }
 }
