511f5e8f92b254a277a10faa15abfd14aa581e99
jotrick <jotrick@microsoft.com>
2019-07-23 12:33:51 -0700

    Added more parameters to run method signature

--- Source/Microsoft.Teams.Apps.CompanyCommunicator.Send.Func/Function1.cs
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Send.Func/Function1.cs
@@ -7,8 +7,15 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Send.Func
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static void Run([ServiceBusTrigger("company-communicator-send", Connection = "ServiceBusConnection")]string myQueueItem, ILogger log)
+        [FunctionName("CompanyCommunicatorSend")]
+        public static void Run(
+            [ServiceBusTrigger("company-communicator-send", Connection = "ServiceBusConnection")]
+            string myQueueItem,
+            Int32 deliveryCount,
+            DateTime enqueuedTimeUtc,
+            string messageId,
+            ILogger log,
+            ExecutionContext context)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
         }
