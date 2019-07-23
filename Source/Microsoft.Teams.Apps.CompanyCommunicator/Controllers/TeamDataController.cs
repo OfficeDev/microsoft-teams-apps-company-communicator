096881850a4c6f389e30a3d38bec1316cd8c45ad
Hong Xu <v-hongxu@microsoft.com>
2019-07-23 11:06:05 -0700

    Added GetTeamDataByIdsAsync method in TeamDataController.

--- Source/Microsoft.Teams.Apps.CompanyCommunicator/Controllers/TeamDataController.cs
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator/Controllers/TeamDataController.cs
@@ -4,7 +4,9 @@
 
 namespace Microsoft.Teams.Apps.CompanyCommunicator.Controllers
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
@@ -17,7 +19,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Controllers
     /// </summary>
     [Route("api/teamData")]
     [Authorize(PolicyNames.MustBeValidUpnPolicy)]
-    public class TeamDataController
+    public class TeamDataController : ControllerBase
     {
         private readonly TeamDataRepository teamDataRepository;
 
@@ -50,6 +52,41 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Controllers
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Get data for teams by Ids.
+        /// </summary>
+        /// <param name="idsInString">Team ids in string format.</param>
+        /// <returns>A list of team data matching the incoming Ids.
+        /// If the passed in ids are invalid, it returns 404 not found error.</returns>
+        [HttpGet("{idsInString}")]
+        public async Task<ActionResult<IEnumerable<TeamData>>> GetTeamDataByIdsAsync(string idsInString)
+        {
+            if (string.IsNullOrWhiteSpace(idsInString))
+            {
+                return this.NotFound();
+            }
+
+            var ids = idsInString
+                .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(p => p.Trim());
+
+            var result = new List<TeamData>();
+            foreach (var id in ids)
+            {
+                var teamData = new TeamData();
+                teamData.TeamId = id;
+                var teamDataEntity = await this.teamDataRepository.GetAsync(PartitionKeyNames.Metadata.TeamData, id);
+                if (teamDataEntity != null)
+                {
+                    teamData.Name = teamDataEntity.Name;
+                }
+
+                result.Add(teamData);
+            }
+
+            return this.Ok(result);
         }
     }
 }
