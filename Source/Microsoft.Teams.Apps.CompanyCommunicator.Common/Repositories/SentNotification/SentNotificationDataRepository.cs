771cb56cc8b0abd35268fb7b3b05964824268b2e
jotrick <jotrick@microsoft.com>
2019-07-23 12:25:33 -0700

    Added comment

--- Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Repositories/SentNotification/SentNotificationDataRepository.cs
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Repositories/SentNotification/SentNotificationDataRepository.cs
@@ -7,7 +7,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SentNotif
     using Microsoft.Extensions.Configuration;
 
     /// <summary>
-    /// Respository of the notification data.
+    /// Respository of the notification data in the table storage.
     /// </summary>
     public class SentNotificationDataRepository : BaseRepository<SentNotificationDataEntity>
     {
