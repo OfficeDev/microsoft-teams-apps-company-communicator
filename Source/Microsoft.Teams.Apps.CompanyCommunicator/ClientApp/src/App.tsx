d7dcd256c937c09ab4502f0ffdc11051feae67a2
amyzhao <v-yuzha4@microsoft.com>
2019-08-09 17:49:46 -0700

    set up theme

--- Source/Microsoft.Teams.Apps.CompanyCommunicator/ClientApp/src/App.tsx
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator/ClientApp/src/App.tsx
@@ -7,24 +7,88 @@ import StatusTaskModule from './components/StatusTaskModule/statusTaskModule';
 import './App.scss';
 import { Provider, themes } from '@stardust-ui/react';
 import SendConfirmationTaskModule from './components/SendConfirmationTaskModule/sendConfirmationTaskModule';
+import * as microsoftTeams from "@microsoft/teams-js";
 
-class App extends React.Component {
+export interface IAppState {
+  theme: string;
+}
+
+class App extends React.Component<{}, IAppState> {
+  private app: any;
+
+  constructor(props: {}) {
+    super(props);
+    this.app = this;
+    this.state = {
+      theme: "",
+    }
+  }
+
+  public componentDidMount() {
+    microsoftTeams.initialize();
+    microsoftTeams.getContext((context) => {
+      this.app.setState({
+        theme: context.theme
+      });
+    });
+
+    microsoftTeams.registerOnThemeChangeHandler((theme) => {
+      this.setState({
+        theme: theme,
+      }, () => {
+        this.forceUpdate();
+      });
+    });
+  }
+
+  public setTheme = () => {
+    if (this.state.theme === "default") {
+      return (
+        <Provider theme={themes.teams}>
+          <div className="defaultContainer">
+            {this.getAppDom()}
+          </div>
+        </Provider>
+      );
+    }
+    else if (this.state.theme === "dark") {
+      return (
+        <Provider theme={themes.teamsDark}>
+          {this.getAppDom()}
+        </Provider>
+      );
+    }
+    else if (this.state.theme === "contrast") {
+      return (
+        <Provider theme={themes.teamsHighContrast}>
+          {this.getAppDom()}
+        </Provider>
+      );
+    }
+  }
+
+  public getAppDom = () => {
+    return (
+      <div className="appContainer">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/configtab" component={Configuration} />
+            <Route exact path="/messages" component={TabContainer} />
+            <Route exact path="/newmessage" component={NewMessage} />
+            <Route exact path="/newmessage/:id" component={NewMessage} />
+            <Route exact path="/viewstatus/:id" component={StatusTaskModule} />
+            <Route exact path="/sendconfirmation/:id" component={SendConfirmationTaskModule} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+
   public render(): JSX.Element {
     return (
-      <Provider theme={themes.teams}>
-        <div className="appContainer">
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/configtab" component={Configuration} />
-              <Route exact path="/messages" component={TabContainer} />
-              <Route exact path="/newmessage" component={NewMessage} />
-              <Route exact path="/newmessage/:id" component={NewMessage} />
-              <Route exact path="/viewstatus/:id" component={StatusTaskModule} />
-              <Route exact path="/sendconfirmation/:id" component={SendConfirmationTaskModule} />
-            </Switch>
-          </BrowserRouter>
-        </div>
-      </Provider>
+      <div>
+        {this.setTheme()}
+      </div>
     );
   }
 }
