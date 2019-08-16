7aa11d7ffc6932c4b386d574fb9f346a89c7ec77
Hong Xu <v-hongxu@microsoft.com>
2019-08-16 16:21:19 -0700

    Revert "Stop sending "Hello and weclome" adaptive card to users after the CC app being installed in Teams."
    This reverts commit 13e468258bdc68304b6c19ede35b01ddfc24ed8c.

--- Source/Microsoft.Teams.Apps.CompanyCommunicator/Bot/CompanyCommunicatorBot.cs
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator/Bot/CompanyCommunicatorBot.cs
@@ -5,6 +5,7 @@
 namespace Microsoft.Teams.Apps.CompanyCommunicator.Bot
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
@@ -44,6 +45,53 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Bot
         }
 
         /// <summary>
+        /// The bot framework calls the method when a new member (human user) is added.
+        /// </summary>
+        /// <param name="membersAdded">A collection of added members.</param>
+        /// <param name="turnContext">ITurnContext instance.</param>
+        /// <param name="cancellationToken">CancellationToken instance.</param>
+        /// <returns>A task that represents the work queued to execute.</returns>
+        protected override async Task OnMembersAddedAsync(
+            IList<ChannelAccount> membersAdded,
+            ITurnContext<IConversationUpdateActivity> turnContext,
+            CancellationToken cancellationToken)
+        {
+            await base.OnMembersAddedAsync(membersAdded, turnContext, cancellationToken);
+
+            foreach (var member in membersAdded)
+            {
+                // Take action if this event includes user being added
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await this.SendAsync(turnContext, cancellationToken, "Hello and Welcome!");
+                }
+            }
+        }
+
+        /// <summary>
+        /// The bot framework calls the method when a member (human user) is removed.
+        /// </summary>
+        /// <param name="membersRemoved">A collection of removed members.</param>
+        /// <param name="turnContext">ITurnContext instance.</param>
+        /// <param name="cancellationToken">CancellationToken instance.</param>
+        /// <returns>A task that represents the work queued to execute.</returns>
+        protected override async Task OnMembersRemovedAsync(
+            IList<ChannelAccount> membersRemoved,
+            ITurnContext<IConversationUpdateActivity> turnContext,
+            CancellationToken cancellationToken)
+        {
+            await base.OnMembersRemovedAsync(membersRemoved, turnContext, cancellationToken);
+
+            foreach (var member in membersRemoved)
+            {
+                // Take action if this event includes user being removed
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                }
+            }
+        }
+
+        /// <summary>
         /// Invoked when a conversation update activity is received from the channel.
         /// </summary>
         /// <param name="turnContext">The context object for this turn.</param>
@@ -74,6 +122,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Bot
             if (activity.MembersAdded?.FirstOrDefault(p => p.Id == botId) != null)
             {
                 await this.teamsDataCapture.OnBotAddedAsync(activity);
+                await this.SendAsync(turnContext, cancellationToken, "Hello and Welcome!");
             }
 
             // Take action if this event includes the bot being removed
@@ -97,6 +146,23 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Bot
             }
 
             return CompanyCommunicatorBot.TeamRenamedEventType.Equals(channelData.EventType, StringComparison.OrdinalIgnoreCase);
+        }
+
+        private async Task SendAsync(
+            ITurnContext<IConversationUpdateActivity> turnContext,
+            CancellationToken cancellationToken,
+            string message)
+        {
+            var card = new HeroCard
+            {
+                Title = "Company Communicator",
+                Subtitle = "Powered by Microsoft Bot Framework",
+                Text = message,
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Get Started", value: "https://docs.microsoft.com/bot-framework") },
+            };
+
+            var reply = MessageFactory.Attachment(card.ToAttachment());
+            await turnContext.SendActivityAsync(reply, cancellationToken);
         }
     }
 }
