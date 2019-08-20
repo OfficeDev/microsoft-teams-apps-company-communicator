6c03e236f00213d117e9588d540ebdfb24521976
Adrian Solis <aosolis@microsoft.com>
2019-08-19 19:48:49 -0700

    Skip audit when installing npm packages in Release

--- deploy.app.cmd
+++ deploy.app.cmd
@@ -74,10 +74,10 @@ IF !ERRORLEVEL! NEQ 0 goto error
 :: 2. Restore npm packages
 echo Restoring npm packages (this can take several minutes)
 pushd "%DEPLOYMENT_SOURCE%\Source\Microsoft.Teams.Apps.CompanyCommunicator\ClientApp"
-call :ExecuteCmd npm install
+call :ExecuteCmd npm install --no-audit
 IF !ERRORLEVEL! NEQ 0 (
     echo First attempt failed, retrying once
-    call :ExecuteCmd npm install
+    call :ExecuteCmd npm install --no-audit
 )
 popd
 IF !ERRORLEVEL! NEQ 0 goto error
