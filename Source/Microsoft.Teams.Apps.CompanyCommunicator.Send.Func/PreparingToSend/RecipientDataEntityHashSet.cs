ac5eebd86b48e2ccc67cb92f5556231db0bc1b46
Hong Xu <v-hongxu@microsoft.com>
2019-09-23 14:51:24 -0700

    Revert "Remove unused code."
    This reverts commit 2a3cdef1788d6cffba805d353235931b90af5752.

---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Send.Func/PreparingToSend/RecipientDataEntityHashSet.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="RecipientDataEntityHashSet.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Send.Func.PreparingToSend
+{
+    using System.Collections.Generic;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.UserData;
+
+    /// <summary>
+    /// Recipient data entity hash set.
+    /// This class helps to get rid of duplicate records in a recipient data list.
+    /// </summary>
+    public class RecipientDataEntityHashSet : HashSet<UserDataEntity>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RecipientDataEntityHashSet"/> class.
+        /// </summary>
+        /// <param name="userDataEntityList">User data entity list.</param>
+        public RecipientDataEntityHashSet(IEnumerable<UserDataEntity> userDataEntityList)
+            : base(userDataEntityList, new EqualityComarer())
+        {
+        }
+
+        private class EqualityComarer : IEqualityComparer<UserDataEntity>
+        {
+            public bool Equals(UserDataEntity x, UserDataEntity y)
+            {
+                return x.AadId == y.AadId;
+            }
+
+            public int GetHashCode(UserDataEntity userDataEntity)
+            {
+                return userDataEntity.AadId.GetHashCode();
+            }
+        }
+    }
+}
