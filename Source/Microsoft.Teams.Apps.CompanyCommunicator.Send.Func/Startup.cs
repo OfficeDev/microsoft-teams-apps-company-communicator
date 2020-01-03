ba76650e65cab6cc78f4b77d18093ea182708c47
jotrick <jotrick@microsoft.com>
2020-01-03 14:54:41 -0600

    Added Nuget packages and alterations for dependency injection.

--- Source/Microsoft.Teams.Apps.CompanyCommunicator.Send.Func/Startup.cs
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Send.Func/Startup.cs
@@ -8,9 +8,12 @@
 namespace Microsoft.Teams.Apps.CompanyCommunicator.Send.Func
 {
     using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.AdaptiveCard;
-    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.BotConnectorClient;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.NotificationData;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SentNotificationData;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.UserData;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueue;
 
     /// <summary>
@@ -21,9 +24,24 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Send.Func
         /// <inheritdoc/>
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddSingleton<BotConnectorClientFactory>();
+            builder.Services.AddHttpClient();
 
-            builder.Services.AddTransient<AdaptiveCardCreator>();
+            // This option is injected as IOptions<RepositoryOptions>.
+            builder.Services.AddOptions<RepositoryOptions>()
+                .Configure<IConfiguration>((repositoryOptions, configuration) =>
+                {
+                    // Set the default to indicate this is an Azure Function.
+                    repositoryOptions.IsAzureFunction = true;
+
+                    // Bind any matching configuration settings to corresponding
+                    // values in the options.
+                    configuration.Bind(repositoryOptions);
+                });
+
+            builder.Services.AddSingleton<SendingNotificationDataRepository>();
+            builder.Services.AddSingleton<GlobalSendingNotificationDataRepository>();
+            builder.Services.AddSingleton<UserDataRepository>();
+            builder.Services.AddSingleton<SentNotificationDataRepository>();
 
             builder.Services.AddSingleton<SendQueue>();
             builder.Services.AddSingleton<DataQueue>();
