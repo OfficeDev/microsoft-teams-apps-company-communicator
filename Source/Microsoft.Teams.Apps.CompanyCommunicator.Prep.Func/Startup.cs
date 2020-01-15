d58a223cfd95a70718f52ddfa4f40308b244a959
jotrick <jotrick@microsoft.com>
2020-01-14 20:09:14 -0800

    Start of adding the dependency injection

--- Source/Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func/Startup.cs
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func/Startup.cs
@@ -8,15 +8,19 @@
 namespace Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func
 {
     using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.NotificationData;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SentNotificationData;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.TeamData;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.UserData;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.AdaptiveCard;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.BotConnectorClient;
-    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueue;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueues;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueues.DataQueue;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueues.SendQueue;
     using Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func.PreparingToSend;
     using Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func.PreparingToSend.GetRecipientDataBatches;
     using Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func.PreparingToSend.SendTriggersToAzureFunctions;
@@ -29,23 +33,38 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func
         /// <inheritdoc/>
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            builder.Services.AddOptions<BotOptions>()
+                .Configure<IConfiguration>((botOptions, configuration) =>
+                {
+                    ////
+                });
             builder.Services.AddSingleton<BotConnectorClientFactory>();
 
-            builder.Services.Configure<RepositoryOptions>(repositoryOptions =>
-            {
-                repositoryOptions.IsAzureFunction = true;
-            });
+            builder.Services.AddOptions<RepositoryOptions>()
+                .Configure<IConfiguration>((repositoryOptions, configuration) =>
+                {
+                    ////repositoryOptions.IsItExpectedThatTableAlreadyExists = true;
+                });
+            builder.Services.AddTransient<TableRowKeyGenerator>();
             builder.Services.AddSingleton<NotificationDataRepository>();
             builder.Services.AddSingleton<SendingNotificationDataRepository>();
             builder.Services.AddSingleton<SentNotificationDataRepository>();
             builder.Services.AddSingleton<UserDataRepository>();
             builder.Services.AddSingleton<TeamDataRepository>();
-            builder.Services.AddTransient<TableRowKeyGenerator>();
 
+            builder.Services.AddOptions<MessageQueueOptions>()
+                .Configure<IConfiguration>((messageQueueOptions, configuration) =>
+                {
+                    ////
+                });
             builder.Services.AddSingleton<SendQueue>();
             builder.Services.AddSingleton<DataQueue>();
 
+
+
             builder.Services.AddTransient<AdaptiveCardCreator>();
+
+
 
             builder.Services.AddTransient<PreparingToSendOrchestration>();
             builder.Services.AddTransient<GetRecipientDataListForAllUsersActivity>();
