cc9ff4fc445c0a346861da2975926dd382641abe
jotrick <jotrick@microsoft.com>
2020-04-22 19:35:34 -0700

    Removed unnecessary properties from Data queue message

--- Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Services/MessageQueues/DataQueue/DataQueueMessageContent.cs
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Services/MessageQueues/DataQueue/DataQueueMessageContent.cs
@@ -4,29 +4,6 @@
 
 namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueues.DataQueue
 {
-    using System;
-
-    /// <summary>
-    /// Result types for notifications in the data queue.
-    /// </summary>
-    public enum DataQueueResultType
-    {
-        /// <summary>
-        /// The notification was successfully sent.
-        /// </summary>
-        Succeeded,
-
-        /// <summary>
-        /// The notification was throttled.
-        /// </summary>
-        Throttled,
-
-        /// <summary>
-        /// The notificaiton failed to be sent.
-        /// </summary>
-        Failed,
-    }
-
     /// <summary>
     /// Azure service bus data queue message content class.
     /// </summary>
@@ -38,18 +15,8 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueues
         public string NotificationId { get; set; }
 
         /// <summary>
-        /// Gets or sets the sent DateTime of the corresponding notification.
-        /// </summary>
-        public DateTime? SentDate { get; set; }
-
-        /// <summary>
-        /// Gets or sets the result of the corresponding notification.
-        /// </summary>
-        public DataQueueResultType ResultType { get; set; }
-
-        /// <summary>
         /// Gets or sets a value indicating whether the data function should force the
-        /// corresponding notification to be complete.
+        /// corresponding notification to be marked as complete.
         /// </summary>
         public bool ForceMessageComplete { get; set; }
     }
