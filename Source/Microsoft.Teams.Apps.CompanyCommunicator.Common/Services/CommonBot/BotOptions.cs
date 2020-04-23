d3cbf2241849426ef853dd14749bd1eff8781c1e
jotrick <jotrick@microsoft.com>
2020-04-22 19:32:46 -0700

    Removed CommonBotAdapter; moved BotOptions

--- Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Services/CommonBot/BotOptions.cs
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Services/CommonBot/BotOptions.cs
@@ -2,7 +2,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // </copyright>
 
-namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Services
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.CommonBot
 {
     /// <summary>
     /// Options used for holding metadata for the bot.
