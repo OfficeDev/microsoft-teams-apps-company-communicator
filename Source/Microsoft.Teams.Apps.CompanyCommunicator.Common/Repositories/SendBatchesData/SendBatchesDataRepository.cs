e1a7c785e309776196e1f0908028e8cb36131e8f
jotrick <jotrick@microsoft.com>
2020-04-30 02:25:47 -0700

    Added the SendBatchesData repository

---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Repositories/SendBatchesData/SendBatchesDataRepository.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="SendBatchesDataRepository.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SendBatchesData
+{
+    using Microsoft.Extensions.Options;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SentNotificationData;
+
+    /// <summary>
+    /// Repository for the send batches data in the table storage.
+    /// </summary>
+    public class SendBatchesDataRepository : BaseRepository<SentNotificationDataEntity>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SendBatchesDataRepository"/> class.
+        /// </summary>
+        /// <param name="repositoryOptions">Options used to create the repository.</param>
+        public SendBatchesDataRepository(IOptions<RepositoryOptions> repositoryOptions)
+            : base(
+                storageAccountConnectionString: repositoryOptions.Value.StorageAccountConnectionString,
+                tableName: SendBatchesDataTableNames.TableName,
+                defaultPartitionKey: SendBatchesDataTableNames.DefaultPartition,
+                isItExpectedThatTableAlreadyExists: repositoryOptions.Value.IsItExpectedThatTableAlreadyExists)
+        {
+        }
+    }
+}
---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Repositories/SendBatchesData/SendBatchesDataTableNames.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="SendBatchesDataTableNames.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SendBatchesData
+{
+    /// <summary>
+    /// Send batches data table names.
+    /// </summary>
+    public static class SendBatchesDataTableNames
+    {
+        /// <summary>
+        /// Table name for the send batches data table.
+        /// </summary>
+        public static readonly string TableName = "SendBatchesData";
+
+        /// <summary>
+        /// Default partition - should not be used.
+        /// </summary>
+        public static readonly string DefaultPartition = "Default";
+    }
+}
