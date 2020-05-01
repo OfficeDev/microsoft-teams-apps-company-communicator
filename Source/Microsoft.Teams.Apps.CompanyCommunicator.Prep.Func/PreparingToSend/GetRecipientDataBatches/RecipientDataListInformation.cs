9ef6f785864e9ad6f6f185c40b5d48d4940e9138
jotrick <jotrick@microsoft.com>
2020-04-30 21:46:31 -0700

    Added class to hold data

---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func/PreparingToSend/GetRecipientDataBatches/RecipientDataListInformation.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="RecipientDataListInformation.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func.PreparingToSend.GetRecipientDataBatches
+{
+    /// <summary>
+    /// Class to hold the recipient data list information.
+    /// </summary>
+    public class RecipientDataListInformation
+    {
+        /// <summary>
+        /// Gets or sets the total number of recipients.
+        /// </summary>
+        public int TotalNumberOfRecipients { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of recipient data batches.
+        /// </summary>
+        public int NumberOfRecipientDataBatches { get; set; }
+    }
+}
