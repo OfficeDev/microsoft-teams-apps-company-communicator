adf4dcb1e6e75ffa1ce15d123d4f53cd37f3d75b
jotrick <jotrick@microsoft.com>
2020-04-30 23:43:36 -0700

    [v2] Add data func pr (#156)
    Merged in changes for the Data function project

---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Data.Func/Services/NotificationDataServices/AggregateSentNotificationDataService.cs
@@ -0,0 +1,96 @@
+﻿// <copyright file="AggregateSentNotificationDataService.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Data.Func.Services.NotificationDataServices
+{
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos.Table;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SentNotificationData;
+
+    /// <summary>
+    /// A service that fetches and aggregates the Sent Notification Data results.
+    /// </summary>
+    public class AggregateSentNotificationDataService
+    {
+        private readonly SentNotificationDataRepository sentNotificationDataRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateSentNotificationDataService"/> class.
+        /// </summary>
+        /// <param name="sentNotificationDataRepository">The sent notification data repository.</param>
+        public AggregateSentNotificationDataService(SentNotificationDataRepository sentNotificationDataRepository)
+        {
+            this.sentNotificationDataRepository = sentNotificationDataRepository;
+        }
+
+        /// <summary>
+        /// Fetches all of the current known results for the Sent Notification and calculates the various totals
+        /// as results.
+        /// </summary>
+        /// <param name="notificationId">The notification ID.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+        public async Task<AggregatedSentNotificationDataResults> AggregateSentNotificationDataResultsAsync(
+            string notificationId)
+        {
+            var partitionKeyFilter = TableQuery.GenerateFilterCondition(
+                nameof(TableEntity.PartitionKey),
+                QueryComparisons.Equal,
+                notificationId);
+
+            // The query is based on the delivery status types that are currently aggregated.
+            var succeededDeliveryStatusFilter = TableQuery.GenerateFilterCondition(
+                nameof(SentNotificationDataEntity.DeliveryStatus),
+                QueryComparisons.Equal,
+                SentNotificationDataEntity.Succeeded);
+
+            var failedDeliveryStatusFilter = TableQuery.GenerateFilterCondition(
+                nameof(SentNotificationDataEntity.DeliveryStatus),
+                QueryComparisons.Equal,
+                SentNotificationDataEntity.Failed);
+
+            var throttledDeliveryStatusFilter = TableQuery.GenerateFilterCondition(
+                nameof(SentNotificationDataEntity.DeliveryStatus),
+                QueryComparisons.Equal,
+                SentNotificationDataEntity.Throttled);
+
+            // Create the complete query where:
+            // PartitionKey eq notificationId AND
+            //      DeliveryStatus eq Succeeded OR
+            //      DeliveryStatus eq Failed OR
+            //      DeliveryStatus eq Throttled
+            var partialDeliveryStatusFilter = TableQuery.CombineFilters(succeededDeliveryStatusFilter, TableOperators.Or, failedDeliveryStatusFilter);
+            var completeDeliveryStatusFilter = TableQuery.CombineFilters(partialDeliveryStatusFilter, TableOperators.Or, throttledDeliveryStatusFilter);
+            var completeFilter = TableQuery.CombineFilters(partitionKeyFilter, TableOperators.And, completeDeliveryStatusFilter);
+            var query = new TableQuery<SentNotificationDataEntity>().Where(completeFilter);
+
+            try
+            {
+                var aggregatedResults = new AggregatedSentNotificationDataResults();
+
+                TableContinuationToken currentContinuationToken = null;
+
+                do
+                {
+                    // Make the query to the data table and update the continuation token in order to continue to paginate the results.
+                    TableQuerySegment<SentNotificationDataEntity> resultSegment = await this.sentNotificationDataRepository.Table
+                        .ExecuteQuerySegmentedAsync<SentNotificationDataEntity>(query, currentContinuationToken);
+                    currentContinuationToken = resultSegment.ContinuationToken;
+
+                    // Aggregate the results.
+                    foreach (var sentNotification in resultSegment)
+                    {
+                        aggregatedResults.UpdateAggregatedResults(sentNotification);
+                    }
+                }
+                while (currentContinuationToken != null);
+
+                return aggregatedResults;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Data.Func/Startup.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="Startup.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+[assembly: Microsoft.Azure.Functions.Extensions.DependencyInjection.FunctionsStartup(
+    typeof(Microsoft.Teams.Apps.CompanyCommunicator.Data.Func.Startup))]
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Data.Func
+{
+    using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.NotificationData;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SentNotificationData;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueues;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueues.DataQueue;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Data.Func.Services.NotificationDataServices;
+
+    /// <summary>
+    /// Register services in DI container of the Azure functions system.
+    /// </summary>
+    public class Startup : FunctionsStartup
+    {
+        /// <inheritdoc/>
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Add all options set from configuration values.
+            builder.Services.AddOptions<RepositoryOptions>()
+                .Configure<IConfiguration>((repositoryOptions, configuration) =>
+                {
+                    repositoryOptions.StorageAccountConnectionString =
+                        configuration.GetValue<string>("StorageAccountConnectionString");
+
+                    // Defaulting this value to true because the main app should ensure all
+                    // tables exist. It is here as a possible configuration setting in
+                    // case it needs to be set differently.
+                    repositoryOptions.IsItExpectedThatTableAlreadyExists =
+                        configuration.GetValue<bool>("IsItExpectedThatTableAlreadyExists", true);
+                });
+            builder.Services.AddOptions<MessageQueueOptions>()
+                .Configure<IConfiguration>((messageQueueOptions, configuration) =>
+                {
+                    messageQueueOptions.ServiceBusConnection =
+                        configuration.GetValue<string>("ServiceBusConnection");
+                });
+            builder.Services.AddOptions<DataQueueMessageOptions>()
+                .Configure<IConfiguration>((dataQueueMessageOptions, configuration) =>
+                {
+                    dataQueueMessageOptions.FirstTenMinutesRequeueMessageDelayInSeconds =
+                        configuration.GetValue<double>("FirstTenMinutesRequeueMessageDelayInSeconds", 30);
+
+                    dataQueueMessageOptions.RequeueMessageDelayInSeconds =
+                        configuration.GetValue<double>("RequeueMessageDelayInSeconds", 300);
+                });
+
+            // Add notification data services.
+            builder.Services.AddTransient<AggregateSentNotificationDataService>();
+            builder.Services.AddTransient<UpdateNotificationDataService>();
+
+            // Add repositories.
+            builder.Services.AddSingleton<NotificationDataRepository>();
+            builder.Services.AddSingleton<SentNotificationDataRepository>();
+
+            // Add service bus message queues.
+            builder.Services.AddSingleton<DataQueue>();
+        }
+    }
+}
