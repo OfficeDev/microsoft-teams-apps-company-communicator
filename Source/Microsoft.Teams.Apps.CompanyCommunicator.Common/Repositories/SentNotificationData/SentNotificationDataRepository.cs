fff90b8b4d4a978a6a65a3d9d071a02f1b03375d
jotrick <jotrick@microsoft.com>
2020-04-30 22:34:12 -0700

    Removed unused initializers from SentNotificationDataRepository

--- Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Repositories/SentNotificationData/SentNotificationDataRepository.cs
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Repositories/SentNotificationData/SentNotificationDataRepository.cs
@@ -4,12 +4,8 @@
 
 namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SentNotificationData
 {
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.Extensions.Options;
-    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.TeamData;
-    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.UserData;
 
     /// <summary>
     /// Repository of the notification data in the table storage.
@@ -43,65 +39,6 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SentNotif
             {
                 await this.Table.CreateAsync();
             }
-        }
-
-        /// <summary>
-        /// Initializes the sent notification data for a recipient batch of users.
-        /// Sets the status code to be the initialized value.
-        /// </summary>
-        /// <param name="notificationDataEntityId">Notification data entity id.</param>
-        /// <param name="recipientDataBatch">A recipient data batch of users.</param>
-        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
-        public async Task InitializeSentNotificationDataForUserRecipientBatchAsync(
-            string notificationDataEntityId,
-            IEnumerable<UserDataEntity> recipientDataBatch)
-        {
-            // Create the SentNotificationDataEntity from the incoming UserDataEntity.
-            var sentNotificationDataEntities = recipientDataBatch
-                .Select(p =>
-                    new SentNotificationDataEntity
-                    {
-                        PartitionKey = notificationDataEntityId,
-                        RowKey = p.AadId,
-                        RecipientType = SentNotificationDataEntity.UserRecipientType,
-                        RecipientId = p.AadId,
-                        StatusCode = SentNotificationDataEntity.InitializationStatusCode,
-                        ConversationId = p.ConversationId,
-                        TenantId = p.TenantId,
-                        UserId = p.UserId,
-                        ServiceUrl = p.ServiceUrl,
-                    });
-
-            await this.BatchInsertOrMergeAsync(sentNotificationDataEntities);
-        }
-
-        /// <summary>
-        /// Initializes the sent notification data for a recipient batch of teams.
-        /// Sets the status code to be the initialized value.
-        /// </summary>
-        /// <param name="notificationDataEntityId">Notification data entity id.</param>
-        /// <param name="recipientDataBatch">A recipient data batch of teams.</param>
-        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
-        public async Task InitializeSentNotificationDataForTeamRecipientBatchAsync(
-            string notificationDataEntityId,
-            IEnumerable<TeamDataEntity> recipientDataBatch)
-        {
-            // Create the SentNotificationDataEntity from the incoming TeamDataEntity.
-            var sentNotificationDataEntities = recipientDataBatch
-                .Select(p =>
-                    new SentNotificationDataEntity
-                    {
-                        PartitionKey = notificationDataEntityId,
-                        RowKey = p.TeamId,
-                        RecipientType = SentNotificationDataEntity.TeamRecipientType,
-                        RecipientId = p.TeamId,
-                        StatusCode = SentNotificationDataEntity.InitializationStatusCode,
-                        ConversationId = p.TeamId,
-                        TenantId = p.TenantId,
-                        ServiceUrl = p.ServiceUrl,
-                    });
-
-            await this.BatchInsertOrMergeAsync(sentNotificationDataEntities);
         }
     }
 }
