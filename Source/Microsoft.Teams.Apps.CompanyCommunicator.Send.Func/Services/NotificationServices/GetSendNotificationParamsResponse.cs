87cbbd1f51b354b336d6c816645283eb4f41a984
jotrick <jotrick@microsoft.com>
2020-04-30 23:21:15 -0700

    [v2] Refactor send func sdk pr (#155)
    Merged in changes to the Send function project

---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Send.Func/Services/NotificationServices/GetSendNotificationParamsResponse.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="GetSendNotificationParamsResponse.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Send.Func.Services.NotificationServices
+{
+    /// <summary>
+    /// The class to hold the necessary parameters generated/fetched for sending the notification.
+    /// </summary>
+    public class GetSendNotificationParamsResponse
+    {
+        /// <summary>
+        /// Gets or sets the notification content.
+        /// </summary>
+        public string NotificationContent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the recipient's unique identifier.
+        ///     If the recipient is a user, this should be the AAD Id.
+        ///     If the recipient is a team, this should be the team Id.
+        /// </summary>
+        public string RecipientId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the service URL.
+        /// </summary>
+        public string ServiceUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the conversation Id.
+        /// </summary>
+        public string ConversationId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the Azure Function should
+        /// stop processing the message because something negative occurred
+        /// while generating the parameters e.g. getting throttled, a failure, etc.
+        /// </summary>
+        public bool ForceCloseAzureFunction { get; set; }
+    }
+}
---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Send.Func/Startup.cs
@@ -0,0 +1,102 @@
+﻿// <copyright file="Startup.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+[assembly: Microsoft.Azure.Functions.Extensions.DependencyInjection.FunctionsStartup(
+    typeof(Microsoft.Teams.Apps.CompanyCommunicator.Send.Func.Startup))]
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Send.Func
+{
+    using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+    using Microsoft.Bot.Builder.Integration.AspNet.Core;
+    using Microsoft.Bot.Connector.Authentication;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.NotificationData;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SentNotificationData;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.UserData;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.CommonBot;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueues;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueues.SendQueue;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Send.Func.Services.ConversationServices;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Send.Func.Services.DataServices;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Send.Func.Services.NotificationServices;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Send.Func.Services.PrecheckServices;
+
+    /// <summary>
+    /// Register services in DI container of the Azure functions system.
+    /// </summary>
+    public class Startup : FunctionsStartup
+    {
+        /// <inheritdoc/>
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Add all options set from configuration values.
+            builder.Services.AddOptions<CompanyCommunicatorSendFunctionOptions>()
+                .Configure<IConfiguration>((companyCommunicatorSendFunctionOptions, configuration) =>
+                {
+                    companyCommunicatorSendFunctionOptions.MaxNumberOfAttempts =
+                        configuration.GetValue<int>("MaxNumberOfAttempts", 1);
+
+                    companyCommunicatorSendFunctionOptions.SendRetryDelayNumberOfSeconds =
+                        configuration.GetValue<double>("SendRetryDelayNumberOfSeconds", 660);
+                });
+            builder.Services.AddOptions<BotOptions>()
+                .Configure<IConfiguration>((botOptions, configuration) =>
+                {
+                    botOptions.MicrosoftAppId =
+                        configuration.GetValue<string>("MicrosoftAppId");
+
+                    botOptions.MicrosoftAppPassword =
+                        configuration.GetValue<string>("MicrosoftAppPassword");
+                });
+            builder.Services.AddOptions<RepositoryOptions>()
+                .Configure<IConfiguration>((repositoryOptions, configuration) =>
+                {
+                    repositoryOptions.StorageAccountConnectionString =
+                        configuration.GetValue<string>("StorageAccountConnectionString");
+
+                    // Defaulting this value to true because the main app should ensure all
+                    // tables exist. It is here as a possible configuration setting in
+                    // case it needs to be set differently.
+                    repositoryOptions.IsItExpectedThatTableAlreadyExists =
+                        configuration.GetValue<bool>("IsItExpectedThatTableAlreadyExists", true);
+                });
+            builder.Services.AddOptions<MessageQueueOptions>()
+                .Configure<IConfiguration>((messageQueueOptions, configuration) =>
+                {
+                    messageQueueOptions.ServiceBusConnection =
+                        configuration.GetValue<string>("ServiceBusConnection");
+                });
+
+            // Add the precheck service.
+            builder.Services.AddTransient<PrecheckService>();
+
+            // Add the create user conversation service.
+            builder.Services.AddTransient<CreateUserConversationService>();
+
+            // Add the notification services.
+            builder.Services.AddTransient<GetSendNotificationParamsService>();
+            builder.Services.AddTransient<SendNotificationService>();
+            builder.Services.AddTransient<DelaySendingNotificationService>();
+
+            // Add the result data service.
+            builder.Services.AddTransient<ManageResultDataService>();
+
+            // Add bot services.
+            builder.Services.AddSingleton<CommonMicrosoftAppCredentials>();
+            builder.Services.AddSingleton<ICredentialProvider, CommonBotCredentialProvider>();
+            builder.Services.AddSingleton<BotFrameworkHttpAdapter>();
+
+            // Add repositories.
+            builder.Services.AddSingleton<SendingNotificationDataRepository>();
+            builder.Services.AddSingleton<GlobalSendingNotificationDataRepository>();
+            builder.Services.AddSingleton<UserDataRepository>();
+            builder.Services.AddSingleton<SentNotificationDataRepository>();
+
+            // Add service bus message queues.
+            builder.Services.AddSingleton<SendQueue>();
+        }
+    }
+}
