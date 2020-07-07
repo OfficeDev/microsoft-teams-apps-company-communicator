9e635f4def96dd7dbc0e263767aad62a58a14a7c
Priyank Saxena <priyank.saxena@microsoft.com>
2020-07-07 10:34:32 +0530

    adding export table and repo

---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Repositories/ExportData/ExportDataRepository.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="ExportDataRepository.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.ExportData
+{
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Options;
+
+    /// <summary>
+    /// Repository of the export data stored in the table storage.
+    /// </summary>
+    public class ExportDataRepository : BaseRepository<ExportDataEntity>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExportDataRepository"/> class.
+        /// </summary>
+        /// <param name="repositoryOptions">Options used to create the repository.</param>
+        public ExportDataRepository(IOptions<RepositoryOptions> repositoryOptions)
+            : base(
+                storageAccountConnectionString: repositoryOptions.Value.StorageAccountConnectionString,
+                tableName: ExportDataTableName.TableName,
+                defaultPartitionKey: ExportDataTableName.DefaultPartition,
+                isItExpectedThatTableAlreadyExists: repositoryOptions.Value.IsItExpectedThatTableAlreadyExists)
+        {
+        }
+
+        /// <summary>
+        /// This method ensures the SendBatchesData table is created in the storage.
+        /// This method should be called before kicking off an Azure function that uses the SendBatchesData table.
+        /// Otherwise the app will crash.
+        /// By design, Azure functions (in this app) do not create a table if it's absent.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        public async Task EnsureExportDataTableExistsAsync()
+        {
+            var exists = await this.Table.ExistsAsync();
+            if (!exists)
+            {
+                await this.Table.CreateAsync();
+            }
+        }
+    }
+}
---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.Common/Repositories/ExportData/ExportDataTableName.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="ExportDataTableName.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.ExportData
+{
+    /// <summary>
+    /// Export data table names.
+    /// </summary>
+    public class ExportDataTableName
+    {
+        /// <summary>
+        /// Table name for the send batches data table.
+        /// </summary>
+        public static readonly string TableName = "ExportData";
+
+        /// <summary>
+        /// Default partition - should not be used.
+        /// </summary>
+        public static readonly string DefaultPartition = "Default";
+    }
+}
