1149ce1598529327560a140df9bd08bbe7b955a6
Adrian Solis <aosolis@microsoft.com>
2021-06-11 16:04:34 -0700

    Company Communicator v4.1.1 (#423)
    - Update version number in manifests to 4.1.1
    - Fix bug in #420 where it tries to get the member information for the bot, resulting for 404 Not Found errors
    - Ensure Azure tables are created by prep function. This allows a fix for the issue reported in #420 by simply deleting the UserData table and restarting the prep Azure function.
    - Update npm packages

--- Source/CompanyCommunicator.Prep.Func/Startup.cs
+++ Source/CompanyCommunicator.Prep.Func/Startup.cs
@@ -55,7 +55,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func
                     // tables exist. It is here as a possible configuration setting in
                     // case it needs to be set differently.
                     repositoryOptions.EnsureTableExists =
-                        !configuration.GetValue<bool>("IsItExpectedThatTableAlreadyExists", true);
+                        !configuration.GetValue<bool>("IsItExpectedThatTableAlreadyExists", false);
                 });
             builder.Services.AddOptions<BotOptions>()
                 .Configure<IConfiguration>((botOptions, configuration) =>
