cacd82aeaa953e3fcb4706b23621a4e4ff2aa698
Cristiano Almeida Goncalves <cristianoag@hotmail.com>
2021-06-29 17:51:26 -0300

    Fix to ensure image uploads with less than 30 kb

--- Source/CompanyCommunicator/ClientApp/src/components/NewMessage/newMessage.tsx
+++ Source/CompanyCommunicator/ClientApp/src/components/NewMessage/newMessage.tsx
@@ -212,7 +212,7 @@ class NewMessage extends React.Component<INewMessageProps, formState> {
             //resize the image to fit in the adaptivecard
             Resizer.imageFileResizer(file, 400, 400, 'JPEG', 80, 0,
                 uri => {
-                    if (uri.toString().length < 32768) {
+                    if (uri.toString().length < 30720) {
                         //everything is ok with the image, lets set it on the card and update
                         setCardImageLink(this.card, uri.toString());
                         this.updateCard();
