41dc262efa0060d70c7d63aa0ab3a9adbf4fa8e9
Adrian Solis <Adrian.Solis@microsoft.com>
2021-09-26 07:45:07 +0000

    Merged PR 3121: Limit the size of the error and warning messages columns to 1024 characters.
    Limit the size of the error and warning messages columns to 1024 characters.
    Add unit tests to validate.

--- Source/CompanyCommunicator.Common/Repositories/NotificationData/NotificationDataRepository.cs
+++ Source/CompanyCommunicator.Common/Repositories/NotificationData/NotificationDataRepository.cs
@@ -17,6 +17,13 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.Notificat
     public class NotificationDataRepository : BaseRepository<NotificationDataEntity>, INotificationDataRepository
     {
         /// <summary>
+        /// Maximum length of error and warning messages to save in the entity.
+        /// This limit ensures that we don't hit the Azure table storage limits for the max size of the data
+        /// in a column, and the total size of an entity.
+        /// </summary>
+        public const int MaxMessageLengthToSave = 1024;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="NotificationDataRepository"/> class.
         /// </summary>
         /// <param name="logger">The logging service.</param>
@@ -172,8 +179,14 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.Notificat
                 notificationDataEntityId);
             if (notificationDataEntity != null)
             {
-                notificationDataEntity.ErrorMessage =
-                    this.AppendNewLine(notificationDataEntity.ErrorMessage, errorMessage);
+                var newMessage = this.AppendNewLine(notificationDataEntity.ErrorMessage, errorMessage);
+
+                // Restrict the total length of stored message to avoid hitting table storage limits
+                if (newMessage.Length <= MaxMessageLengthToSave)
+                {
+                    notificationDataEntity.ErrorMessage = newMessage;
+                }
+
                 notificationDataEntity.Status = NotificationStatus.Failed.ToString();
 
                 // Set the end date as current date.
@@ -195,8 +208,14 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.Notificat
                     notificationDataEntityId);
                 if (notificationDataEntity != null)
                 {
-                    notificationDataEntity.WarningMessage =
-                        this.AppendNewLine(notificationDataEntity.WarningMessage, warningMessage);
+                    var newMessage = this.AppendNewLine(notificationDataEntity.WarningMessage, warningMessage);
+
+                    // Restrict the total length of stored message to avoid hitting table storage limits
+                    if (newMessage.Length <= MaxMessageLengthToSave)
+                    {
+                        notificationDataEntity.WarningMessage = newMessage;
+                    }
+
                     await this.CreateOrUpdateAsync(notificationDataEntity);
                 }
             }
